546e9a9b212153c10dc809129a5376a8de53e488
John Hannan <john816_@hotmail.com>
2023-01-27 01:03:47 +0800

    Created readable addListener Function.

--- Forge/src/main/java/morethanhidden/infernalnature/InfernalNature.java
+++ Forge/src/main/java/morethanhidden/infernalnature/InfernalNature.java
@@ -2,7 +2,9 @@ package morethanhidden.infernalnature;
 
 import morethanhidden.infernalnature.client.ClientProxy;
 import morethanhidden.infernalnature.registry.InfernalNatureFluids;
+import net.minecraft.core.Registry;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
@@ -14,6 +16,9 @@ import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegisterEvent;
+
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 @Mod(Constants.MOD_ID)
 	public class InfernalNature {
@@ -47,25 +52,20 @@ import net.minecraftforge.registries.RegisterEvent;
 				//MinecraftForge.EVENT_BUS.register(new CraftingHandler());
 				//MinecraftForge.EVENT_BUS.register(new GemOnMineEvent());
 				FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-				FMLJavaModLoadingContext.get().getModEventBus().addListener((RegisterEvent event) -> {
-					if(event.getRegistryKey().equals(ForgeRegistries.FLUIDS.getRegistryKey())){
-						InfernalNatureFluids.registerFluid(((fluid, resourceLocation) ->
-								event.register(ForgeRegistries.FLUIDS.getRegistryKey(), resourceLocation, () -> fluid))
-						);
-					}else if(event.getRegistryKey().equals(ForgeRegistries.BLOCKS.getRegistryKey())){
-						InfernalNatureFluids.registerBlock(((block, resourceLocation) ->
-								event.register(ForgeRegistries.BLOCKS.getRegistryKey(), resourceLocation, () -> block))
-						);
-					}else if(event.getRegistryKey().equals(ForgeRegistries.ITEMS.getRegistryKey())){
-						InfernalNatureFluids.registerItem(((item, resourceLocation) ->
-								event.register(ForgeRegistries.ITEMS.getRegistryKey(), resourceLocation, () -> item))
-						);
-					}
-
-				});
+				addListener(ForgeRegistries.FLUIDS.getRegistryKey(), InfernalNatureFluids::registerFluid);
+				addListener(ForgeRegistries.BLOCKS.getRegistryKey(), InfernalNatureFluids::registerBlock);
+				addListener(ForgeRegistries.ITEMS.getRegistryKey(), InfernalNatureFluids::registerItem);
 				proxy.registerRenderers();
 
 	        }
+
+			private static <T> void addListener(ResourceKey<Registry<T>> registry, Consumer<BiConsumer<T, ResourceLocation>> consumer) {
+				FMLJavaModLoadingContext.get().getModEventBus().addListener((RegisterEvent event) -> {
+					if (registry.equals(event.getRegistryKey())) {
+						consumer.accept((typeObj, resLocation) -> event.register(registry, resLocation, () -> typeObj));
+					}
+				});
+			}
 
 			@SubscribeEvent
 			public void setup(FMLCommonSetupEvent event) {
